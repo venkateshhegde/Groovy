191196f7bac178d46b7a6627f90f292ceda9047b
ven <venkateshhegde@yahoo.com>
2019-07-19 06:41:51 -0400

    Moved files from maven stuff

---
+++ test/Cust.groovy
@@ -0,0 +1,15 @@
+package test
+
+import groovy.transform.Immutable
+import groovy.transform.Sortable
+import groovy.transform.ToString
+
+@Sortable
+@ToString
+@Immutable class Cust {
+
+    String x
+    String y
+
+}
+
